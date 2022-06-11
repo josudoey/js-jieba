a039d19e95d09d6c2cb7f8bedbd6913c1ccde4d2
Joey Fang <josudoey@gmail.com>
2022-06-11 15:28:05 +0800

    feat: add bump version script

---
+++ scripts/bump-version.js
@@ -0,0 +1,109 @@
+const args = require('minimist')(process.argv.slice(2))
+const fs = require('fs')
+const path = require('path')
+const chalk = require('chalk')
+const semver = require('semver')
+const currentVersion = require('../package.json').version
+const { prompt } = require('enquirer')
+
+const preId =
+  args.preid ||
+  (semver.prerelease(currentVersion) && semver.prerelease(currentVersion)[0])
+const packages = fs
+  .readdirSync(path.resolve(__dirname, '../packages'))
+  .filter(p => !p.endsWith('.ts') && !p.startsWith('.'))
+
+
+const versionIncrements = [
+  'patch',
+  'minor',
+  'major',
+  ...(preId ? ['prepatch', 'preminor', 'premajor', 'prerelease'] : [])
+]
+
+const inc = i => semver.inc(currentVersion, i, preId)
+const getPkgRoot = pkg => path.resolve(__dirname, '../packages/' + pkg)
+const step = msg => console.log(chalk.cyan(msg))
+
+async function main() {
+  let targetVersion = args._[0]
+
+  if (!targetVersion) {
+    // no explicit version, offer suggestions
+    const { release } = await prompt({
+      type: 'select',
+      name: 'release',
+      message: 'Select release type',
+      choices: versionIncrements.map(i => `${i} (${inc(i)})`).concat(['custom'])
+    })
+
+    if (release === 'custom') {
+      targetVersion = (
+        await prompt({
+          type: 'input',
+          name: 'version',
+          message: 'Input custom version',
+          initial: currentVersion
+        })
+      ).version
+    } else {
+      targetVersion = release.match(/\((.*)\)/)[1]
+    }
+  }
+
+  if (!semver.valid(targetVersion)) {
+    throw new Error(`invalid target version: ${targetVersion}`)
+  }
+
+  const { yes } = await prompt({
+    type: 'confirm',
+    name: 'yes',
+    message: `Updating v${targetVersion}. Confirm?`
+  })
+
+  if (!yes) {
+    return
+  }
+
+  // update all package versions and inter-dependencies
+  step('\nUpdating cross dependencies...')
+  updateVersions(targetVersion)
+}
+
+function updateVersions(version) {
+  // 1. update root package.json
+  updatePackage(path.resolve(__dirname, '..'), version)
+  // 2. update all packages
+  packages.forEach(p => updatePackage(getPkgRoot(p), version))
+}
+
+function updatePackage(pkgRoot, version) {
+  const pkgPath = path.resolve(pkgRoot, 'package.json')
+  const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
+  pkg.version = version
+  updateDeps(pkg, 'dependencies', version)
+  updateDeps(pkg, 'peerDependencies', version)
+  fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n')
+}
+
+function updateDeps(pkg, depType, version) {
+  const deps = pkg[depType]
+  if (!deps) return
+  Object.keys(deps).forEach(dep => {
+    if (
+      dep === 'vue' ||
+      (dep.startsWith('@vue') && packages.includes(dep.replace(/^@vue\//, '')))
+    ) {
+      console.log(
+        chalk.yellow(`${pkg.name} -> ${depType} -> ${dep}@${version}`)
+      )
+      deps[dep] = version
+    }
+  })
+}
+
+
+main().catch(err => {
+  updateVersions(currentVersion)
+  console.error(err)
+})
