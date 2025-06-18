1ec37f574484ec6cc1c2e7550dce864f52854103
Joey Fang <josudoey@gmail.com>
2025-06-18 16:14:45 +0800

    ci: change deploy gh page

--- packages/web-demo/next.config.ts
+++ packages/web-demo/next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production"
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/js-jieba" : undefined,
-  assetPrefix: isProd ? "/js-jieba/" : undefined,
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
