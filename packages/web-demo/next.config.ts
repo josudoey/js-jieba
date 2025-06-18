e3b327ba4f7caeb0e5e4884062f1ed1e879452c1
Joey Fang <josudoey@gmail.com>
2025-06-18 15:44:20 +0800

    fix: correct web demo next config

--- packages/web-demo/next.config.ts
+++ packages/web-demo/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production"
+
 const nextConfig: NextConfig = {
-  output: "export"
+  output: "export",
+  basePath: isProd ? "/js-jieba" : undefined,
+  assetPrefix: isProd ? "/js-jieba/" : undefined,
 };
 
 export default nextConfig;
